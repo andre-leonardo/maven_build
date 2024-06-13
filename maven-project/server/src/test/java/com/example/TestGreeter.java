fd8e781296439987c0e857c18a2f9895e6136c7d
André Leonardo <98830924+andre-leonardo@users.noreply.github.com>
2024-06-13 20:47:22 -0300

    Update TestGreeter.java

--- maven-project/server/src/test/java/com/example/TestGreeter.java
+++ maven-project/server/src/test/java/com/example/TestGreeter.java
@@ -6,20 +6,7 @@ import org.junit.Test;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.junssert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
-
-public class TestGreeter {
-
-  private Greeter greeter;
-
-  @Before
-  public void setup() {
-    greeter = new Greeter();
-  }
-
-  @Test
-  public void greetShouldIncludeTheOneBeingGreeted() {
-    String soeone = "World";
+impor
 
     assertThat(greeter.greet(someone), containsString(someone));
   }
